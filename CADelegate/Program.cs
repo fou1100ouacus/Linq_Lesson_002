1b533076df6fd415cabe78c30e7e1c37d750a301
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2026-02-09 14:18:10 +0200

    change commit1

--- CADelegate/Program.cs
+++ CADelegate/Program.cs
@@ -2,6 +2,9 @@
 
 namespace CADelegate
 {
+    //dotnet run  Program.cs   
+
+
     //delegate void MDelegate();
     //Action, Func, Pred
     internal class Program
@@ -10,20 +13,33 @@ namespace CADelegate
         {
             // M2(M1);
 
-            Action action = M1;
-            M2(action);
+            // Action action = M1;
+            // M2(action);
+             Action action = M3;
+             M(action);
             Console.ReadKey();
         }
 
-        static void M1()
+        // static void M1()
+        // {
+        //     Console.WriteLine("M1");
+        // }
+
+        static void M3()
         {
-            Console.WriteLine("M1");
+            Console.WriteLine("hi iam aya  ");
         }
 
-        static void M2(Action action)
+        // static void M2(Action action)
+        // {
+        //     action();
+        //     Console.WriteLine("M2");
+        // }
+
+          static void M(Action action)
         {
             action();
-            Console.WriteLine("M2");
+            Console.WriteLine("M3");
         }
     }
 
